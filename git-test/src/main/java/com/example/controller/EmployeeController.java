de474655089e7727cde9ca5c7b8f7a2bef674fcd
Anish <anish.alwekar@gmail.com>
2024-06-09 13:13:37 +0530

    Resolved conflicts

--- git-test/src/main/java/com/example/controller/EmployeeController.java
+++ git-test/src/main/java/com/example/controller/EmployeeController.java
@@ -6,18 +6,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmployeeController {
 
-	@GetMapping
+	@GetMapping("/hello")
 	public String hello() {
 
 		return "hello";
 
 	}
 	
+
 	@GetMapping
 	public String hello6() {
 
 		return "hello6";
 
 	}
+
+	@GetMapping("/hello1")
+	public String hello1() {
+
+		return "hello1";
+
+	}
+	
+	
+	@GetMapping("/hello2")
+	public String hello2() {
+
+		return "hello2";
+
+	}
+	
+	
+
 
 }
